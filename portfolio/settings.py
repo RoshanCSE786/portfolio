0818cc323d3a694b5b13e8fad844436138b78feb
Roshan Sahu <roshansahh786@gmail.com>
2025-12-24 19:41:30 +0530

    Fix static files using Whitenoise

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -130,3 +131,4 @@ STATIC_ROOT = BASE_DIR /'static'
 STATICFILES_DIRS = [
     'Base/static'
 ]
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
